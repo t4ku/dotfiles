24271f1ec12d12e1ec1901151332cccc149ec616
t4ku <taku.okawa@gmail.com>
2012-06-30 12:42:09 +0900

    update install and rc files for oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,14 @@ do
            ln -Fis "$PWD/$dotfile" $HOME
        fi
 done
+
+# oh-my-zsh
+if [ -d 'oh-my-zsh/custom' ]; then
+  if [ ! -L 'oh-my-zsh/custom' ]; then
+    rm -rf 'oh-my-zsh/custom'
+    ln -s zsh/oh-my-zsh/custom oh-my-zsh/custom
+    # git doesn't mind symlink or dir
+    cd oh-my-zsh;
+    git checkout custom/example*
+  fi
+fi
