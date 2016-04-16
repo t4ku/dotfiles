39403c1679d5cd3e49813c6dc9a0789afd600a6d
Taku Okawa <taku.okawa@gmail.com>
2016-04-16 12:32:20 +0900

    Update fzf script

--- .fzf.zsh
+++ .fzf.zsh
@@ -21,8 +21,66 @@ fbr() {
   branch=$(echo "$branches" | fzf +m) &&
   git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
 }
+fz() {
+  local dir
+  dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
+}
 
-v() {
+fv() {
   local file
   file="$(fasd -Rfl "$1" | fzf -1 -0 --no-sort +m)" && vi "${file}" || return 1
 }
+
+
+# http://blog.b4b4r07.com/entry/2015/11/08/013526
+export ONELINER_FILE=$HOME/dotfiles/onliner.txt
+
+exec-oneliner() {
+    local oneliner_f
+    oneliner_f="${ONELINER_FILE:-~/.commnad.list}"
+
+    [[ ! -f $oneliner_f || ! -s $oneliner_f ]] && return
+
+    local cmd q k res accept
+    while accept=0; cmd="$(
+        cat <$oneliner_f \
+            | sed -e '/^#/d;/^$/d' \
+            | perl -pe 's/^(\[.*?\]) (.*)$/$1\t$2/' \
+            | perl -pe 's/(\[.*?\])/\033[31m$1\033[m/' \
+            | perl -pe 's/^(: ?)(.*)$/$1\033[30;47;1m$2\033[m/' \
+            | perl -pe 's/^(.*)([[:blank:]]#[[:blank:]]?.*)$/$1\033[30;1m$2\033[m/' \
+            | perl -pe 's/(!)/\033[31;1m$1\033[m/' \
+            | perl -pe 's/(\|| [A-Z]+ [A-Z]+| [A-Z]+ )/\033[35;1m$1\033[m/g' \
+            | fzf --ansi --multi --no-sort --tac --query="$q" \
+            --print-query --expect=ctrl-v --exit-0
+            )"; do
+        q="$(head -1 <<< "$cmd")"
+        k="$(head -2 <<< "$cmd" | tail -1)"
+        res="$(sed '1,2d;/^$/d;s/[[:blank:]]#.*$//' <<< "$cmd")"
+        [ -z "$res" ] && continue
+        if [ "$k" = "ctrl-v" ]; then
+            vim "$oneliner_f" < /dev/tty > /dev/tty
+        else
+            cmd="$(perl -pe 's/^(\[.*?\])\t(.*)$/$2/' <<<"$res")"
+            if [[ $cmd =~ "!$" || $cmd =~ "! *#.*$" ]]; then
+                accept=1
+                cmd="$(sed -e 's/!.*$//' <<<"$cmd")"
+            fi
+            break
+        fi
+    done
+
+    local len
+    if [[ -n $cmd ]]; then
+        BUFFER="$(tr -d '@' <<<"$cmd" | perl -pe 's/\n/; /' | sed -e 's/; $//')"
+        len="${cmd%%@*}"
+        CURSOR=${#len}
+        if [[ $accept -eq 1 ]]; then
+            zle accept-line
+        fi
+    fi
+    #zle reset-prompt
+    zle redisplay
+}
+zle -N exec-oneliner
+bindkey '^x^x' exec-oneliner
--- .zshrc
+++ .zshrc
@@ -2,6 +2,7 @@ before_zshrc
 
 autoload colors; colors;
 export LSCOLORS="Gxfxcxdxbxegedabagacad"
+export TERM=xterm-256color
 
 # switch color option available. Linux (ls --color), or BSD (ls -G)
 ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
