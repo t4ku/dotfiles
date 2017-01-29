ddb9134cd3be78713f49f98839e24c84a2b56d02
Taku Okawa <taku.okawa@gmail.com>
2017-01-29 17:05:52 +0900

    add helm key bind

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,6 +27,21 @@
 
 (require 'helm-config)
 (helm-mode 1)
+;; helm
+(global-set-key (kbd "C-x b") 'helm-for-files)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+;; helmショートカット
+(global-set-key (kbd "C-x C-h h") 'helm-mini)
+(global-set-key (kbd "C-x C-h r") 'helm-recentf)
+(global-set-key (kbd "C-x C-h i") 'helm-imenu)
+(global-set-key (kbd "C-x C-h k") 'helm-show-kill-ring)
+;; helmコマンド内のキーバインド
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-read-file-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+;;(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+;;(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+
 
 ;;Font設定
 ;;(set-face-attribute 'default nil :family "monaco" :height 110)
