aaa5c7291636bbbae373855cac9f9885c39eb299
Taku Okawa <taku.okawa@gmail.com>
2018-06-24 13:27:35 +0900

    Organize init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,6 +12,8 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+
+
 ;; Cask
 (require 'cask "/usr/local/opt/cask/cask.el")
 (cask-initialize)
@@ -29,9 +31,19 @@
 ;; map Cmd-f1 to change gui window(emacs frames)
 (global-set-key (kbd "<s-f1>") 'other-frame)
 
-;;;;;;;;;;;;;;;;;;;;;
+
+;; like vim's C-o
+(defun switch-to-previous-buffer ()
+  "Switch to previously open buffer.
+Repeated invocations toggle between the two most recently open buffers."
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
+
+(global-set-key (kbd "C-c b") 'switch-to-previous-buffer)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; emacs
-;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; mouse scroll
 (global-set-key [wheel-right] 'scroll-left)
@@ -53,135 +65,11 @@
   kept-old-versions 2
   version-control t)
 
-;;;;;;;;;;;;;;;;;;;;;
-;; plugins
-;;;;;;;;;;;;;;;;;;;;;
 
-(require 'helm-config)
-(helm-mode 1)
-;; helm
-(global-set-key (kbd "C-x b") 'helm-for-files)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-;; helmショートカット
-(global-set-key (kbd "C-x C-h h") 'helm-mini)
-(global-set-key (kbd "C-x C-h r") 'helm-recentf)
-(global-set-key (kbd "C-x C-h i") 'helm-imenu)
-(global-set-key (kbd "C-x C-h k") 'helm-show-kill-ring)
-;; helmコマンド内のキーバインド
-(define-key helm-map (kbd "C-h") 'delete-backward-char)
-(define-key helm-read-file-map (kbd "C-h") 'delete-backward-char)
-(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
-;;(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
-;;(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; appearence
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; set correct path in emacs shell
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
-
-;; set python path in pyenv
-(pyenv-mode)
-
-;; reload inline imge
-;; http://emacs.stackexchange.com/questions/13107/replace-plantuml-source-with-generated-image-in-org-mode
-(add-hook 'org-babel-after-execute-hook
-          (lambda ()
-            (when org-inline-image-overlays
-              (org-redisplay-inline-images))))
-
-(org-babel-do-load-languages
-'org-babel-load-languages
-'((emacs-lisp . t)
-  (ipython . t)
-  (org . t)
-  (sql . t)
-  (latex . t)
-  (sh . t)))
-
-;;org-agenda
-(define-key org-mode-map (kbd "C-c C-a") 'org-agenda-execute)
-(define-key org-mode-map (kbd "C-c s") 'org-schedule)
-(define-key org-mode-map (kbd "C-c d") 'org-deadline)
-
-;; org link
-;; https://emacs.stackexchange.com/questions/19598/org-mode-link-to-heading-in-other-org-file
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c C-l") 'org-insert-link)
-
-;; open link
-
-(require 'org)
-
-(defun my-find-file-fn (file)
-"If the filename extension of FILE ends in `.txt' or `.el' or `.png', then use
-`find-file-other-frame'; otherwise, use `find-file-other-window'.  The three file
-extensions txt/el/png are hard-coded into the let-bound variable `regex'."
-  (let* (
-      ;; regexp-matchp-fn from:  https://github.com/kentaro/auto-save-buffers-enhanced
-      ;; regexp-matchp-fn modified by @sds:  http://stackoverflow.com/a/20343715/2112489
-      (regexp-matchp-fn
-        (lambda (regexps string)
-          (and string
-             (catch 'matched
-               (let ((inhibit-changing-match-data t))
-                 (dolist (regexp regexps)
-                   (when (string-match regexp string)
-                     (throw 'matched t))))))))
-      (ext (file-name-extension file))
-      (regex '("txt" "el" "png")))
-    (if (funcall regexp-matchp-fn regex ext)
-      (find-file-other-frame file)
-      ;;(find-file-other-window file))))
-      (find-file file))))
-
-(setq org-link-frame-setup '(
-  (vm . vm-visit-folder-other-frame)
-  (vm-imap . vm-visit-imap-folder-other-frame)
-  (gnus . org-gnus-no-new-news)
-  (file . my-find-file-fn)
-  (wl . wl-other-frame)))
-
-(setq org-agenda-custom-commands '(
-  ("1" "Events" agenda "display deadlines and exclude scheduled" (
-    (org-agenda-span 'month)
-    (org-agenda-time-grid nil)
-    (org-agenda-show-all-dates nil)
-    (org-agenda-entry-types '(:deadline :scheduled)) ;; this entry excludes :scheduled
-    (org-agenda-skip-scheduled-delay-if-deadline nil)
-    (org-scheduled-delay-days 0)
-    (org-scheduled-past-days 2)
-    (org-deadline-warning-days 0) ))
-    ))
-
-
-;;org-goto
-(setq org-goto-interface 'outline-path-completion
-      org-goto-max-level 10)
-(setq org-outline-path-complete-in-steps nil)
-
-(setq org-agenda-files (list "~/Dropbox/Docs/org/Agenda"))
-
-;;org-todo
-(setq org-todo-keywords
-      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
-              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)"))))
-
-
-(setq org-src-fontify-natively t)
-
-(defface org-block-begin-line
-  '((t (:underline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF")))
-  "Face used for the line delimiting the begin of source blocks.")
-
-(defface org-block-background
-  '((t (:background "#FFFFEA")))
-  "Face used for the source block background.")
-
-(defface org-block-end-line
-  '((t (:overline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF")))
-  "Face used for the line delimiting the end of source blocks.")
-
-;;neotree
-(global-set-key [f8] 'neotree-toggle)
 
 ;;Font設定
 ;;(set-face-attribute 'default nil :family "monaco" :height 110)
@@ -250,5 +138,186 @@ extensions txt/el/png are hard-coded into the let-bound variable `regex'."
 
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; plugins
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'helm-config)
+(helm-mode 1)
+;; helm
+(global-set-key (kbd "C-x b") 'helm-for-files)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+;; helmショートカット
+(global-set-key (kbd "C-x C-h h") 'helm-mini)
+(global-set-key (kbd "C-x C-h r") 'helm-recentf)
+(global-set-key (kbd "C-x C-h i") 'helm-imenu)
+(global-set-key (kbd "C-x C-h k") 'helm-show-kill-ring)
+;; helmコマンド内のキーバインド
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-read-file-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+;;(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+;;(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+
+;; set correct path in emacs shell
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; -------------
+;; smartparens
+;; -------------
+
+(smartparens-global-mode t)
+
+;; -------------
+;; pyenv
+;; -------------
+
+;; set python path in pyenv
+(pyenv-mode)
+
+;; -------------
+;;neotree
+;; -------------
+(global-set-key [f8] 'neotree-toggle)
+
+;; -------------
 ;; auto-complete
+;; -------------
 (ac-config-default)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; org-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; reload inline imge
+;; http://emacs.stackexchange.com/questions/13107/replace-plantuml-source-with-generated-image-in-org-mode
+(add-hook 'org-babel-after-execute-hook
+          (lambda ()
+            (when org-inline-image-overlays
+              (org-redisplay-inline-images))))
+
+;; org language
+(org-babel-do-load-languages
+'org-babel-load-languages
+'((emacs-lisp . t)
+  (ipython . t)
+  (org . t)
+  (sql . t)
+  (latex . t)
+  (sh . t)))
+
+;;org-protocol
+(require 'org-protocol)
+;; start emacs server so that emacsclient can connect
+;; when using org-protocol
+;;(server-start)
+
+;;org-capture
+(define-key global-map "\C-cc" 'org-capture)
+
+;;capture-template
+;;(setq org-capture-templates
+;;      '(("t" "Todo" entry (file+headline "~/Dropbox/org/Agenda/work.org" "Capture")
+;;             "* TODO %?n %in %a")
+;;        ("l" "Logbook" entry (file+datetree "~/Dropbox/org/Journal/logbook-work.org")
+;;             "* %?n %Un %in %a")
+;;        ("n" "Note" entry (file+headline "~/Dropbox/org/notes.org" "Notes")
+;;             "* %?n %Un %i")
+;;
+;;org-agenda
+(define-key org-mode-map (kbd "C-c C-a") 'org-agenda-execute)
+(define-key org-mode-map (kbd "C-c s") 'org-schedule)
+(define-key org-mode-map (kbd "C-c d") 'org-deadline)
+
+;; org link
+;; https://emacs.stackexchange.com/questions/19598/org-mode-link-to-heading-in-other-org-file
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c C-l") 'org-insert-link)
+
+;; key mapping
+;;(add-hook 'org-mode-hook
+;;	  (lambda ()
+;;	    (local-set-key (kbd "C-c C-u") #'outline-up-heading)))
+;;
+;; C-c C-u (original keymap) was overritten by pymode-env set
+;; add similar keymap
+(global-set-key (kbd "C-c u") 'outline-up-heading)
+
+;; open link
+
+(require 'org)
+
+(defun my-find-file-fn (file)
+"If the filename extension of FILE ends in `.txt' or `.el' or `.png', then use
+`find-file-other-frame'; otherwise, use `find-file-other-window'.  The three file
+extensions txt/el/png are hard-coded into the let-bound variable `regex'."
+  (let* (
+      ;; regexp-matchp-fn from:  https://github.com/kentaro/auto-save-buffers-enhanced
+      ;; regexp-matchp-fn modified by @sds:  http://stackoverflow.com/a/20343715/2112489
+      (regexp-matchp-fn
+        (lambda (regexps string)
+          (and string
+             (catch 'matched
+               (let ((inhibit-changing-match-data t))
+                 (dolist (regexp regexps)
+                   (when (string-match regexp string)
+                     (throw 'matched t))))))))
+      (ext (file-name-extension file))
+      (regex '("txt" "el" "png")))
+    (if (funcall regexp-matchp-fn regex ext)
+      (find-file-other-frame file)
+      ;;(find-file-other-window file))))
+      (find-file file))))
+
+(setq org-link-frame-setup '(
+  (vm . vm-visit-folder-other-frame)
+  (vm-imap . vm-visit-imap-folder-other-frame)
+  (gnus . org-gnus-no-new-news)
+  (file . my-find-file-fn)
+  (wl . wl-other-frame)))
+
+
+
+;; custome agenda view
+(setq org-agenda-custom-commands '(
+  ("1" "Events" agenda "display deadlines and exclude scheduled" (
+    (org-agenda-span 'month)
+    (org-agenda-time-grid nil)
+    (org-agenda-show-all-dates nil)
+    (org-agenda-entry-types '(:deadline :scheduled)) ;; this entry excludes :scheduled
+    (org-agenda-skip-scheduled-delay-if-deadline nil)
+    (org-scheduled-delay-days 0)
+    (org-scheduled-past-days 2)
+    (org-deadline-warning-days 0) ))
+    ))
+
+
+;;org-goto
+(setq org-goto-interface 'outline-path-completion
+      org-goto-max-level 10)
+(setq org-outline-path-complete-in-steps nil)
+
+(setq org-agenda-files (list "~/Dropbox/Docs/org/Agenda"))
+
+;;org-todo
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)"))))
+
+
+(setq org-src-fontify-natively t)
+
+(defface org-block-begin-line
+  '((t (:underline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF")))
+  "Face used for the line delimiting the begin of source blocks.")
+
+(defface org-block-background
+  '((t (:background "#FFFFEA")))
+  "Face used for the source block background.")
+
+(defface org-block-end-line
+  '((t (:overline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF")))
+  "Face used for the line delimiting the end of source blocks.")
+
+
