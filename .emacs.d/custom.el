b9ca5c68ed922f966e5c5df353b778233a8f866f
Taku Okawa <taku.okawa@gmail.com>
2018-09-07 15:55:15 +0900

    Add emacs custom.el

---
+++ .emacs.d/custom.el
@@ -0,0 +1,58 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#ecf0f1" "#e74c3c" "#2ecc71" "#f1c40f" "#2492db" "#9b59b6" "#1abc9c" "#2c3e50"])
+ '(custom-enabled-themes (quote (leuven)))
+ '(custom-safe-themes
+   (quote
+    ("9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "5cd0afd0ca01648e1fff95a7a7f8abec925bd654915153fb39ee8e72a8b56a1f" "4156d0da4d9b715c6f7244be34f2622716fb563d185b6facedca2c0985751334" "5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" "ec13410d459f1b67158c500d13d290560fc4dad2edaaa22e33a4d1df08e8f887" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(fci-rule-color "#f1c40f")
+ '(hl-paren-background-colors (quote ("#2492db" "#95a5a6" nil)))
+ '(hl-paren-colors (quote ("#ecf0f1" "#ecf0f1" "#c0392b")))
+ '(hl-sexp-background-color "#efebe9")
+ '(leuven-scale-outline-headlines nil)
+ '(package-selected-packages
+   (quote
+    (ob-ipython anti-zenburn-theme zenburn-theme leuven-theme espresso-theme flatui-theme tango-plus-theme color-theme-sanityinc-tomorrow yasnippet web-mode use-package smex smartparens projectile prodigy popwin pallet nyan-mode multiple-cursors magit idle-highlight-mode htmlize helm flycheck-cask expand-region exec-path-from-shell drag-stuff)))
+ '(sml/active-background-color "#34495e")
+ '(sml/active-foreground-color "#ecf0f1")
+ '(sml/inactive-background-color "#dfe4ea")
+ '(sml/inactive-foreground-color "#34495e")
+ '(vc-annotate-background "#ecf0f1")
+ '(vc-annotate-color-map
+   (quote
+    ((30 . "#e74c3c")
+     (60 . "#c0392b")
+     (90 . "#e67e22")
+     (120 . "#d35400")
+     (150 . "#f1c40f")
+     (180 . "#d98c10")
+     (210 . "#2ecc71")
+     (240 . "#27ae60")
+     (270 . "#1abc9c")
+     (300 . "#16a085")
+     (330 . "#2492db")
+     (360 . "#0a74b9"))))
+ '(vc-annotate-very-old-color "#0a74b9"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-source-header ((t (:height 1))))
+ '(org-agenda-date ((t (:inherit org-agenda-structure :foreground "#1662AF" :weight bold :height 1))) t)
+ '(org-agenda-date-today ((t (:inherit org-agenda-date :background "#FFFFCC" :foreground "#4F4A3D" :slant italic :weight bold :height 1.0))) t)
+ '(org-agenda-date-weekend ((t (:inherit org-agenda-date :foreground "#4E4E4E" :weight bold :height 1))) t)
+ '(org-agenda-structure ((t (:height 1.0))))
+ ;;'(org-default ((t (:height 1))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1))))
+ '(org-level-6 ((t (:inherit outline-6 :height 1)))))
