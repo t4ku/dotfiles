5169df0e6ed633163ee056950796b827fa2d4dbf
Taku Okawa <taku.okawa@gmail.com>
2019-08-24 21:15:33 +0900

    Use company-mode for english auto-completion
     https://emacs.stackexchange.com/questions/18304/optimal-settings-for-auto-complete-for-writing-papers-prose-in-natural-languag

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -571,3 +571,27 @@ extensions txt/el/png are hard-coded into the let-bound variable `regex'."
 
 (use-package ox-hugo
   :after ox)
+
+;; ---------------
+;; company-mode
+;; ---------------
+
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+
+(setq company-dabbrev-downcase 0)
+(setq company-idle-delay 0)
+
+(defun text-mode-hook-setup ()
+  ;; make `company-backends' local is critcal
+  ;; or else, you will have completion in every major mode, that's very annoying!
+  (make-local-variable 'company-backends)
+
+  ;; company-ispell is the plugin to complete words
+  (add-to-list 'company-backends 'company-ispell)
+
+  ;; OPTIONAL, if `company-ispell-dictionary' is nil, `ispell-complete-word-dict' is used
+  ;;  but I prefer hard code the dictionary path. That's more portable.
+  (setq company-ispell-dictionary (file-truename "~/.emacs.d/misc/english-words.txt")))
+
+(add-hook 'text-mode-hook 'text-mode-hook-setup)
