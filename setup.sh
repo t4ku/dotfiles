3dcfdde05c0c45d4989d94ca7212c483acc9d54e
Taku Okawa <taku.okawa@gmail.com>
2021-12-04 01:36:08 +0900

    Vim setup

--- setup.sh
+++ setup.sh
@@ -18,6 +18,11 @@ done
 # config
 ln -Ffs "$HOME/.config/nvim" "$HOME/dotfiles/nvim"
 
+# vim-plug
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
 which brew &> /dev/null
 if [[ $? > 0 ]]; then 
     echo 'installing homebrew'
@@ -35,6 +40,11 @@ fi
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+anyenv install pyenv
+anyenv install nodenv
+anyenv install rbenv
+
 
 # mitmproxy
 # > mitmdump
