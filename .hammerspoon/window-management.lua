f1617f5748649e981b2419340eee802a18f6bcd2
Taku Okawa <taku.okawa@gmail.com>
2021-12-09 01:40:11 +0900

    window movement/focus movement with yabai/hammerspoon

--- .hammerspoon/window-management.lua
+++ .hammerspoon/window-management.lua
@@ -1,10 +1,23 @@
+local log = hs.logger.new('utils', 'info')
+-- HyperKey.app doing the similar effects as karabiner elements in the artilce
+-- https://prodtyping.com/blog/hyperkey-hammerspoon
+local hyper = {"cmd", "alt", "ctrl","shift"}
 local function moveFocusedWindowToNextScreen()
         local win = hs.window.focusedWindow()
         local screen = win:screen()
-        win:move(win:frame():toUnitRect(screen:frame(), screen:next(), true, 0))
+        -- https://www.hammerspoon.org/docs/hs.window.html#moveToScreen
+        win:moveToScreen(screen:next())
 end
 
+
+local function logMessage()
+        log.i('pressed abc')
+end
+
+
 -- bind hotkey
-hs.hotkey.bind({'hyper', 'cmd'}, 'n', moveFocusedWindowToNextScreen)
+-- hs.hotkey.bind('hyper', 'n', moveFocusedWindowToNextScreen)
+--hs.hotkey.bind({'cmd','alt','ctrl','shift'}, 'n', moveFocusedWindowToNextScreen)
+hs.hotkey.bind(hyper, 'n', moveFocusedWindowToNextScreen)
 
 return moveFocusedWindowToNextScreen
