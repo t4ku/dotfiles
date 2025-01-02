78f350fe18d5a60ecd5d28aa51e0f897bf623b1b
Taku Okawa <taku.okawa@gmail.com>
2025-01-02 13:25:53 +0900

    add nvim lualine

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -3,12 +3,12 @@ return {
   -- Git
   {
     'tpope/vim-fugitive',
-    cmd = { "G", "Git", "GBrowse" },
-    keys = {
-      { "<leader>gs", "<cmd>Git<cr>", desc = "Fugitive status" },
-      { "<leader>gb", "<cmd>GBrowse<cr>", desc = "Fugitive GBrowse" },
-      -- { "gl", "<cmd>Gitv!<cr>", desc = "Gitv" },
-    },
+    -- cmd = { "G", "Git", "GBrowse" },
+    -- keys = {
+    --   { "<leader>gs", "<cmd>Git<cr>", desc = "Fugitive status" },
+    --   { "<leader>gb", "<cmd>GBrowse<cr>", desc = "Fugitive GBrowse" },
+    --   -- { "gl", "<cmd>Gitv!<cr>", desc = "Gitv" },
+    -- },
   },
   -- {
   --   'gbrlsnchs/vim-gitv',
@@ -45,6 +45,56 @@ return {
       vim.keymap.set('n', '<leader>nf', ':NvimTreeFindFile<CR>', { desc = 'Find current file in explorer' })
     end,
   },
+  -- nvim/lua/plugins/lualine.lua
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      -- https://github.com/nvim-lualine/lualine.nvim?tab=readme-ov-file#default-configuration
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = 'auto',
+          component_separators = { left = '', right = ''},
+          section_separators = { left = '', right = ''},
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          always_show_tabline = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 100,
+            tabline = 100,
+            winbar = 100,
+          }
+        },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {'filename'},
+          lualine_x = {'location'},
+          lualine_y = {},
+          lualine_z = {}
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {}
+      }
+    end
+  }
+    
   -- Other smaller plugins
   -- { 'tpope/vim-surround' },
   -- { 'tpope/vim-commentary' },
