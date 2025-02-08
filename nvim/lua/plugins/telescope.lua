46a690fa5854548de07c37444548c8fed4c1105b
Taku Okawa <taku.okawa@gmail.com>
2025-02-08 13:11:02 +0900

    feat: add telescope command to list keymaps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,6 +6,60 @@ return {
         'cljoly/telescope-repo.nvim',
     },
     config = function()
+        local pickers = require('telescope.pickers')
+        local finders = require('telescope.finders')
+        local conf = require('telescope.config').values
+
+        -- Define the keymap search function
+        local function keymap_finder()
+            local modes = {'n', 'i', 'v', 'x', 's', 'o', '!', 'c', 't'}
+            local all_maps = {}
+
+            for _, mode in ipairs(modes) do
+                local mode_maps = vim.api.nvim_get_keymap(mode)
+                for _, map in ipairs(mode_maps) do
+                    if map.desc or map.rhs then
+                        table.insert(all_maps, {
+                            mode = mode,
+                            lhs = map.lhs,
+                            rhs = map.rhs,
+                            desc = map.desc or map.rhs,
+                        })
+                    end
+                end
+            end
+
+            local function format_keymap(map)
+                local mode_labels = {
+                    n = "NORMAL", i = "INSERT", v = "VISUAL",
+                    x = "VISUAL", s = "SELECT", o = "OP-PENDING",
+                    ['!'] = "INS/CMD", c = "COMMAND", t = "TERMINAL"
+                }
+                return string.format("%-8s %-20s %s", 
+                    mode_labels[map.mode] or map.mode,
+                    map.lhs:gsub("<", "\\<"):gsub(">", "\\>"),
+                    map.desc or "")
+            end
+
+            pickers.new({}, {
+                prompt_title = "Key Mappings",
+                finder = finders.new_table({
+                    results = all_maps,
+                    entry_maker = function(entry)
+                        return {
+                            value = entry,
+                            display = format_keymap(entry),
+                            ordinal = format_keymap(entry),
+                        }
+                    end,
+                }),
+                sorter = conf.generic_sorter({}),
+            }):find()
+        end
+
+        -- Register the command
+        vim.api.nvim_create_user_command('Maps', keymap_finder, {})
+
         require('telescope').setup {
             defaults = {
                 -- Your telescope settings
@@ -39,5 +93,6 @@ return {
         { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
         { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
         { "<leader>fr", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+        { "<leader>fm", "<cmd>Maps<cr>", desc = "Search Key Mappings" },
     }
 }
